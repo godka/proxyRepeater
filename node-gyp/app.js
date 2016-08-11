9f8321388d0adeac7ea0710747a48db58fb14bba
zhongkui <zhongkui@dev.gz.cmcc>
2016-08-11 17:04:12 +0800

    node-gyp directory commit
    Signed-off-by: zhongkui <zhongkui@dev.gz.cmcc>

---
+++ node-gyp/app.js
@@ -0,0 +1,22 @@
+var nvr = require('./build/Release/node_nvr_addon.node');
+
+var data = [{
+        url: "rtsp://10.196.230.149/000100",
+        endpoint: "rtmp://117.135.196.137:11935/live",
+        stream: "stream1",
+        password: "publish2016"
+   }, {
+        url: "rtsp://10.196.230.138:8554/cam",
+        endpoint: "rtmp://117.135.196.137:11935/live",
+        stream: "stream2",
+        password: "publish2016"
+}];
+
+nvr.init(JSON.stringify(data), function(err, msg){
+	if(err) {
+		console.error(msg);
+		return;
+	}
+	
+	nvr.start();
+});
